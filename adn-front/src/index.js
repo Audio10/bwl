750538503ad2816a38789b4d0d7c7b564885b895
Claudio Vazquez <claudiojesus_10@hotmail.com>
2019-12-22 09:40:32 -0600

    solo backend

--- adn-front/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import "bootstrap/dist/css/bootstrap.css"
-import App from './components/App'
-
-const container = document.getElementById('app')
-
-ReactDom.render(<App/>, container)
